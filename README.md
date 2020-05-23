4c64e6b6338042e35d805987df5fab6398d44ce4
antoHero <veeqanto@gmail.com>
2020-05-23 19:53:16 +0100

    updated README.md and added sql dump file

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 A simple housing management system. This system seeks to eliminate middlemen/house agents to allow houseowners to deal with their clients directly.
-To get started, download the poject zip file and extract to your computer or your localhost (htdocs folder in XAMPP). Go to phpmyadmin and create a database 'her_project'. Credentials will be supplied later.
+To get started, download the poject zip file and extract to your computer or your localhost (htdocs folder in XAMPP). Go to phpmyadmin and create a database 'her_project'. Import the .sql file from the dump folder. When import is complete, go to your browser and enter 127.0.0.1/project.
+
+For admin, go to 127.0.0.1/project/admin 
+Admin login details
+email: admin@hms.com
+password: 1234567
+
 
 It uses a free template from colorlib. Pay for template before you remove footer credits.
---
+++ dump/her_project_2020-05-23.sql
@@ -0,0 +1,70 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 5446
+#
+# https://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: localhost (MySQL 8.0.19)
+# Database: her_project
+# Generation Time: 2020-05-23 18:49:18 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+SET NAMES utf8mb4;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table listing
+# ------------------------------------------------------------
+
+CREATE TABLE `listing` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `price` int NOT NULL,
+  `bedrooms` int NOT NULL,
+  `location` varchar(100) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `description` text,
+  `type` varchar(50) DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `bathroom` int DEFAULT NULL,
+  `area` varchar(30) DEFAULT NULL,
+  `garage` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `user_type` int DEFAULT NULL,
+  `phone` varchar(20) NOT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  `address` text NOT NULL,
+  `about` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
